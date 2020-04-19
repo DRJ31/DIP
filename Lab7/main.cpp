1d77bc72b7baf1d3a419c9cde99b7c5f92543074
dengrenjie <dengrenjie31@gmail.com>
2020-04-19 22:08:37 +0800

    Finish hw5

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -3,6 +3,22 @@
 
 using namespace std;
 using namespace cv;
+using namespace lab;
+
+void hw5() 
+{
+  Mat src = imread("img/hw5.png");
+  Mat dst = Mat(src.size(), CV_8UC1);
+  ostringstream os;
+  int sizes[] = {3,5,9};
+
+  for (auto size : sizes) {
+    Filter<uchar>::convolution(src, dst, Size(size, size), Filter<uchar>::geometricMean);
+    os.str("");
+    os << "gMean" << size << ".jpg";
+    imwrite(os.str(), dst);
+  }
+}
 
 
 int selection()
@@ -11,6 +27,7 @@ int selection()
   cout << "Select an operation: " << endl;
   cout << "1. Question1" << endl;
   cout << "2. Question2" << endl;
+  cout << "3. Homework5" << endl;
   cout << "Your selection: ";
   cin >> selected;
   return selected;
@@ -26,6 +43,8 @@ void operation(int selected)
     case 2:
       lab::FilterLena();
       break;
+    case 3:
+      hw5();
     default:
       break;
   }
